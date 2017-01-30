f3b771c258ae71aec2b1d68f492184312c745eea
Fabio Vasconcelos <fa.vasconc@gmail.com>
2017-01-30 16:24:58 -0300

    Atualização 30/01.

--- Ferrari.cpp
+++ Ferrari.cpp
@@ -45,12 +45,35 @@ Ferrari::mensagem() const{
 }
 
 void Ferrari::mostrarVelocidade(){
-    
     cout << "Velocidade:" << velocidade << '\n';
 }
 
 void Ferrari::ligarCarro(){
-    
     cout << "Status: " << ligado << '\n';
+}
+
+/*void Ferrari::initId(){
+    for (int i=0; i<sizeId; i++)
+        id[i]=o;
+}*/
+
+void Ferrari::mostraId() const{         //Array
+    for (int i=0; i<sizeId; i++)
+        cout << id[i] << '\t';
+        
+    cout << '\n';
+}
+
+int Data::checarDia(int dia) const{
+    static const int diasPorMes[13]= {0,31,28,31,30,30,31,31,30,31,30,31};
+    if (dia>0 && dia<=diasPorMes[mes])
+        return dia;
+        
+    if (mes==2 && dia==29 && (ano % 400==0 || (ano % 4==0 && ano % 100!=0)))
+        return dia;
+        
+    cout << "Dia inválido.\n";
+    
+    return 1;
 }
 
--- Ferrari.h
+++ Ferrari.h
@@ -1,8 +1,22 @@
 #ifndef FERRARI_H
 #define FERRARI_H
+#ifndef DATA_H_
+#define DATA_H_
 
-class Ferrari
-{
+class Data{
+public:
+    Data(int=1, int=1, int=1900);
+    void print() const;
+    ~Data();
+    
+private:
+    int checarDia(int) const;
+    int dia;
+    int mes;
+    int ano;
+}
+
+class Ferrari{
 public:
     Ferrari();
     ~Ferrari();
@@ -22,6 +36,10 @@ private:
     static int numeroFerraris;
     
     const static double VelocidadeMaxima = 300;
+    
+    const static int sizeId = 10;
+    
+    int id[sizeId];
     
 };
 
